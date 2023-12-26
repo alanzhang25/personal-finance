23e7df4bec4e486ff2e3407c0cfb594339b8ebab
Alan <alanzhang2021@gmail.com>
2023-12-26 12:43:52 -0800

    jupyter notebook file

--- futures_calculator.py
+++ futures_calculator.py
@@ -1,1 +1,17 @@
-print("Hello")
+from sympy import symbols, Eq, solve
+
+def market_drop(leveraged_x, new_leveraged_x):
+    amount = 10000.
+    leveraged_amount = amount * leveraged_x
+
+    x = symbols('x')
+    equation = Eq((leveraged_amount - x) / (amount - x), new_leveraged_x)
+    total_amount_drop = solve(equation, x)[0]
+    return total_amount_drop / amount
+
+
+def upro_mes_portfolio(curr_lev):
+    amount = 10000.
+    
+
+print(market_drop(3, 4))
